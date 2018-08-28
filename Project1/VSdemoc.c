55f82267e1e7b81704b6dc06d66210b47a8f89e4
fyang21117 <yf21117@gmail.com>
2018-08-28 19:51:07 +0800

    0829

--- Project1/VSdemoc.c
+++ Project1/VSdemoc.c
@@ -9,53 +9,97 @@
 #pragma comment(lib, "libeng.lib")
 #pragma comment(lib, "libmx.lib")
 #pragma comment(lib, "libmat.lib")
+int  init_engine(Engine *p);
+void handling(Engine *ep, char temp[]);
+int hexstr2dec(char s[]);
 
 int main()
 {
-	Engine *ep;												
-	int i =0, j =0, k =0;
-	mxArray *testdata = NULL, *result = NULL;
-	char kind[5], level[5], buffer[BUFSIZE + 1],data[240];
-	kind[4] = level[4]= '\0';
-	if (!(ep = engOpen(NULL)))
+	Engine *ep= engOpen(NULL);
+	init_engine(ep);
+
+//	char input[] = { "AAAA007C060103012539030125390301243903012539030125390301243903012539030125390301243903012539030125390301243903012539030125390301243903012539030125390301243903012539030125390301243903012539030125390301243903012539030125390301243903012539030125390301243903020303" };
+	char input[255] ;
+	for (int i = 0; i < 255; i++)
+		input[i] = '0';
+	handling(ep,input);
+
+	engClose(ep);
+	getchar();
+	while (1);								
+	return EXIT_SUCCESS;
+}
+int hexstr2dec(char s[])
+{
+	int i, m, temp=0, n;
+	m = strlen(s);
+	for (i = 0; i<m - 1; i++)
+	{
+		if (s[i] >= 'A'&&s[i] <= 'F')
+			n = s[i] - 'A' + 10;
+		else if (s[i] >= 'a'&&s[i] <= 'f')
+			n = s[i] - 'a' + 10;
+		else
+			n = s[i] - '0';
+		temp = n;
+		i++;
+		if (s[i] >= 'A'&&s[i] <= 'F')
+			n = s[i] - 'A' + 10;
+		else if (s[i] >= 'a'&&s[i] <= 'f')
+			n = s[i] - 'a' + 10;
+		else
+			n = s[i] - '0';
+		temp = temp * 16 + n;
+		printf("%d\n", temp);
+	}	
+	return temp;
+}
+
+int init_engine(Engine *p)
+{
+	if (!p)
 	{
 		printf("Can't start MATLAB engine!\n\n");
 		return EXIT_FAILURE;
 	}
-	engSetVisible(ep, false);
+	engSetVisible(p, false);
+	return 0;
+}
 
-	char input[] = { "ABCDEFGHIJKLMNABCDEFGHIJKLMN012539030125390301243903012539030125390301243903012539030125390301243903012539030125390301243903012539030125390301243903012539030125390301243903012539030125390301243903012539030125390301243903012539030125390301243903012539030125390301243903" };
-	for (i = 28; i < 269; i++,j++)										//截取气味数据到数组
-		data[j] = input[i];
+void handling(Engine *ep, char temp[])
+{
+	int i = 0, j = 0;
+	mxArray *testdata = NULL, *result = NULL;
+	char  level[5], buffer[BUFSIZE + 1], data[241];
+	level[4] = '\0';
+	for (i = 14; i < 255; i++, j++)										//截取气味数据到数组
+		data[j] = temp[i];
+	data[240] = '\0';
 
 	testdata = mxCreateString(data);
 	engPutVariable(ep, "input_data", testdata);							//将testdata传入Matlab工作空间，取名input_data	
 	engEvalString(ep, "save ('data.mat','input_data');");				//生成路径D:\Program Files (x86)\SetupDir\MATLAB\R2016b
 
-	buffer[BUFSIZE] =  '\0' ;
+	buffer[BUFSIZE] = '\0';
 	engOutputBuffer(ep, buffer, BUFSIZE);								//结果缓冲区buffer
-	engEvalString(ep, "stringtest");									// 调用"stringtest.m"
-	engOutputBuffer(ep, NULL, 0);												
-	
+	engEvalString(ep, "hextest");									// 调用"hextest.m"
+	engOutputBuffer(ep, NULL, 0);
+
 	if ((result = engGetVariable(ep, "level")) != NULL)
 	{
-//		printf("The  result of MATLAB is %s", buffer);
-		for (i = 0; i < 4; i++)
+		//for(i=0;i<30;i++)			printf("The  result of MATLAB is %c\n", buffer[i]);
+		for (i = 0; i < 2; i++)
 		{
-			kind[i] = buffer[i + 9];
-			level[i] = buffer[i+25];					// 0x01(smoke),0x02(jinshuqi), 0x03(xibanshui);
+			level[i] = buffer[i + 11];
+			level[i+2] = buffer[i + 27];					// 0x01(smoke),0x02(jinshuqi), 0x03(xibanshui);01-05level
 		}
-		printf("The kind is %s", kind);
-		printf("\nThe level is %s", level);
+		printf("\nThe return is %s", level);
 	}
 	else
-		 printf("engGetVariable error !\n" );
-
-	mxDestroyArray(testdata);							
+		printf("engGetVariable error !\n");
+	mxDestroyArray(testdata);
 	mxDestroyArray(result);
-	engClose(ep);
-	getchar();	while (1);								
-	return EXIT_SUCCESS;
+//	return level;
 }
 
 
