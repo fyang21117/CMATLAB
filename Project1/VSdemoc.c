97b1b4cf6566bafe7e8c4ed1e90bcf309772e7df
fyang21117 <yf21117@gmail.com>
2018-08-29 08:38:24 +0800

    829new

--- Project1/VSdemoc.c
+++ Project1/VSdemoc.c
@@ -11,7 +11,6 @@
 #pragma comment(lib, "libmat.lib")
 int  init_engine(Engine *p);
 void handling(Engine *ep, char temp[]);
-int hexstr2dec(char s[]);
 
 int main()
 {
@@ -28,31 +27,6 @@ int main()
 	getchar();
 	while (1);								
 	return EXIT_SUCCESS;
-}
-int hexstr2dec(char s[])
-{
-	int i, m, temp=0, n;
-	m = strlen(s);
-	for (i = 0; i<m - 1; i++)
-	{
-		if (s[i] >= 'A'&&s[i] <= 'F')
-			n = s[i] - 'A' + 10;
-		else if (s[i] >= 'a'&&s[i] <= 'f')
-			n = s[i] - 'a' + 10;
-		else
-			n = s[i] - '0';
-		temp = n;
-		i++;
-		if (s[i] >= 'A'&&s[i] <= 'F')
-			n = s[i] - 'A' + 10;
-		else if (s[i] >= 'a'&&s[i] <= 'f')
-			n = s[i] - 'a' + 10;
-		else
-			n = s[i] - '0';
-		temp = temp * 16 + n;
-		printf("%d\n", temp);
-	}	
-	return temp;
 }
 
 int init_engine(Engine *p)
@@ -103,53 +77,7 @@ void handling(Engine *ep, char temp[])
 }
 
 
-//			kind[0] = buffer[9]; kind[1] = buffer[10]; kind[2] = buffer[11]; kind[3] = buffer[12]; kind[4] = '\0';
-//			level[0] = buffer[25]; level[1] = buffer[26]; level[2] = buffer[27]; level[3] = buffer[28]; level[4] = '\0';
-//			printf("the buffer is %c\n",buffer[i]);
-
-/*char cat[]= "0x",**stop = NULL,hex[120] ;
-for (i = 15; i < 135; i++)
-{
-sprintf(hex, "%s%d",cat,temp[i]);
-printf(" hex:%4s ", hex);
-cArray[j] = strtol(hex,stop, 16);
-printf(" dec:%3.0lf\n",cArray[j]);
-j++;
-}
-*/
 //	mexPutArray(testdata,"input_data");
 //	testdata = mxCreateDoubleMatrix(1, N, mxREAL);
 //	memcpy((double*)mxGetPr(testdata),(double*)cArray, N * sizeof(double));
 //	memcpy((char*)mxGetPr(testdata), (char*)cArray, N * sizeof(char));
-
-
-//	len = sizeof(cArray)/sizeof(double);
-//	printf("\nthe length is %d \n",len);
-//		printf("The  result of MATLAB is %s", buffer);
-//		sprintf(str,"%s%d%s","\"", HEX_data[i],"\"");
-//		printf("%d \n", atoi(str));
-//	char a = '10';
-//		printf("%d",a-'0');
-//		printf("%d ", strtol(str, NULL, 10));
-//		printf("%d \n",str-'0');
-//		cArray[j] = ((int)str / 10) * 16 + ((int)str % 10);
-//	printf("%3.0lf ", cArray[j]);
-/*
-int c[10],hex[10];
-char str[10];
-double res[10];
-for(i=0;i<2;i++)
-{
-scanf("%s",str);
-printf("%s ",str);
-
-sprintf(str, "%d",  hex[i]);
-printf(" str:%s ",str);
-c[i] = strtol(str, NULL, 10);
-printf(" num:%d ", c[i]);
-res[i] = (c[i] / 10) * 16 + (c[i] % 10);
-printf(" dec:%3.0lf\n",res[i]);
-}*/
-//		strcat(cat,str);//	printf(" %s ", cat);
-//		cArray[j] = (c[j] / 10) * 16 + (c[j] % 10);
-//		cArray[j] = c[j];
